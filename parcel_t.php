8c7b9a259d1272722a6bcd55c64703c47c69953b
Dinura <dinura.sen25@gmail.com>
2025-04-24 22:04:26 +0530

    Search function commit

--- parcel_t.php
+++ parcel_t.php
@@ -1,13 +1,28 @@
+<?php 
+session_start(); 
+include('config.php');
+
+// Handle search input
+$search = "";
+if (isset($_GET['search'])) {
+    $search = mysqli_real_escape_string($conn, $_GET['search']);
+    $sqlSelect = "SELECT * FROM parcel WHERE dropoff LIKE '%$search%'";
+} else {
+    $sqlSelect = "SELECT * FROM parcel";
+}
+$result = mysqli_query($conn, $sqlSelect);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Parcel List</title>
     <link rel="manifest" href="manifest.json">
     <link rel="icon" href="images/1.png" type="image/png">
-    <link rel="stylesheet" href="styles3.css">
+    <link rel="stylesheet" href="styles3.css?v=1.0">
+
+
 </head>
 <body>
     <div class="container">
@@ -17,64 +32,65 @@
                 <a href="main.php">Back</a>
             </div>
         </header>
-        
-        <?php session_start(); ?>
+
         <?php if (isset($_SESSION["create"])) { ?>
-            <div class="alert"> <?php echo $_SESSION["create"]; ?> </div>
-            <?php unset($_SESSION["create"]); } ?>
+            <div class="alert"><?php echo $_SESSION["create"]; unset($_SESSION["create"]); ?></div>
+        <?php } ?>
         <?php if (isset($_SESSION["update"])) { ?>
-            <div class="alert"> <?php echo $_SESSION["update"]; ?> </div>
-            <?php unset($_SESSION["update"]); } ?>
+            <div class="alert"><?php echo $_SESSION["update"]; unset($_SESSION["update"]); ?></div>
+        <?php } ?>
         <?php if (isset($_SESSION["delete"])) { ?>
-            <div class="alert"> <?php echo $_SESSION["delete"]; ?> </div>
-            <?php unset($_SESSION["delete"]); } ?>
-        
+            <div class="alert"><?php echo $_SESSION["delete"]; unset($_SESSION["delete"]); ?></div>
+        <?php } ?>
+
+        <!-- Search Bar -->
+        <form class="search-bar" method="GET" action="">
+            <input type="text" name="search" placeholder="Search by Dropoff Location" value="<?php echo htmlspecialchars($search); ?>">
+            <button type="submit">Search</button>
+        </form>
+
+        <!-- Data Table -->
         <table>
-        <thead>
-            <tr>
-                <th>Parcel ID</th>
-                <th>Sender NIC</th>
-                <th>Receiver NIC</th>
-                <th>Receiver Name</th>
-                <th>Phone Number</th>
-                <th>Pickup Location</th>
-                <th>Drop Station</th>
-                <th>Weight (kg)</th>
-                <th>Status</th>
-                <th>Options</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            
-            <!-- Fetch data dynamically from database -->
-            <?php
-            include('config.php');
-            $sqlSelect = "SELECT * FROM parcel";
-            $result = mysqli_query($conn, $sqlSelect);
-            while ($data = mysqli_fetch_array($result)) {
-            ?>
-            <tr>
-                <td><?php echo $data['parcel_id']; ?></td>
-                <td><?php echo $data['sender_nic']; ?></td>
-                <td><?php echo $data['rec_nic']; ?></td>
-                <td><?php echo $data['rec_name']; ?></td>
-                <td><?php echo $data['rec_phone']; ?></td>
-                <td><?php echo $data['pickup']; ?></td>
-                <td><?php echo $data['dropoff']; ?></td>
-                <td><?php echo $data['weight']; ?></td>
-                <td><?php echo $data['status']; ?></td>
-                <td>
-                    <a href="edit.php?parcel_id=<?php echo $data['parcel_id']; ?>" class="button warning">Edit</a>
-                    <br>
-                    <br>
-                    <a href="delete.php?id=<?php echo $data['parcel_id']; ?>" class="button danger">Delete</a>
-                </td>
-            </tr>
-            <?php } ?>
-        </tbody>
+            <thead>
+                <tr>
+                    <th>Parcel ID</th>
+                    <th>Sender NIC</th>
+                    <th>Receiver NIC</th>
+                    <th>Receiver Name</th>
+                    <th>Phone Number</th>
+                    <th>Pickup Location</th>
+                    <th>Drop Station</th>
+                    <th>Weight (kg)</th>
+                    <th>Status</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($data = mysqli_fetch_array($result)) { ?>
+                <tr>
+                    <td><?php echo $data['parcel_id']; ?></td>
+                    <td><?php echo $data['sender_nic']; ?></td>
+                    <td><?php echo $data['rec_nic']; ?></td>
+                    <td><?php echo $data['rec_name']; ?></td>
+                    <td><?php echo $data['rec_phone']; ?></td>
+                    <td><?php echo $data['pickup']; ?></td>
+                    <td><?php echo $data['dropoff']; ?></td>
+                    <td><?php echo $data['weight']; ?></td>
+                    <td><?php echo $data['status']; ?></td>
+                    <td>
+                        <a href="edit.php?parcel_id=<?php echo $data['parcel_id']; ?>" class="button warning">Edit</a>
+                        <br><br>
+                        <a href="delete.php?id=<?php echo $data['parcel_id']; ?>" class="button danger">Delete</a>
+                    </td>
+                </tr>
+                <?php } ?>
+                <?php if (mysqli_num_rows($result) === 0) { ?>
+                <tr>
+                    <td colspan="10">No parcels found for the given dropoff location.</td>
+                </tr>
+                <?php } ?>
+            </tbody>
         </table>
-
     </div>
 </body>
 </html>
